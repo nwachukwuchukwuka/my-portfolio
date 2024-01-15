2ba51f1b7ca5f7dafd5ff749fba433a89afd080c
nwachukwuchukwuka <nwachukwuadinoyi@gmail.com>
2024-01-15 17:30:03 +0100

    changes to the all the images again and the navbar

--- src/components/ui-cards/Hero.jsx
+++ src/components/ui-cards/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <img className="w-[230px] h-[230px] rounded-tl-[40px] md:rounded-tl-3xl rounded-br-3xl mr-[20px] ml-[15px] pt-[15px] md:pt-0" src={myPics} alt="my-image" />
             <div className="ml-[10px] mt-[25px]">
                 <p className="text-[14px] text-[#7A7A7A;]">A SOFTWARE ENGINEER</p>
-                <p>hello</p>
+                <p>hello how are you</p>
                 <h1 className="text-[30px] font-semibold"> Chukwuka  <br /> Nwachukwu </h1>
                 <p className="text-[#7A7A7A]">I am a web developer based in san fransisco</p>
                 <div className='flex justify-end mt-[10px] md:mt-o'>
